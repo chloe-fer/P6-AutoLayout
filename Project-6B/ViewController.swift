515de2dc22506b6f77c12f5710c977c87a9b7c94
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-08-15 13:32:00 +0800

    Initial Commit

---
+++ Project-6B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-6B
+//
+//  Created by Chloe Fermanis on 15/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
